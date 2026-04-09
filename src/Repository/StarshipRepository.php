2265ef85863c71d711d8176c61187de775cda969
Ester Diez <ediez@c17.net>
2026-04-09 12:34:44 +0200

    añadido enums PHP

--- src/Repository/StarshipRepository.php
+++ src/Repository/StarshipRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repository;
 
 use App\Model\Starship;
+use App\Model\StarshipStatusEnum;
 use Psr\Log\LoggerInterface;
 
 class StarshipRepository
@@ -10,6 +11,7 @@ class StarshipRepository
     public function __construct(private LoggerInterface $logger)
     {
     }
+
     public function findAll(): array
     {
         $this->logger->info('Starship collection retrieved');
@@ -20,31 +22,33 @@ class StarshipRepository
                 'USS LeafyCruiser (NCC-0001)',
                 'Garden',
                 'Jean-Luc Pickles',
-                'taken over by Q'
+                StarshipStatusEnum::IN_PROGRESS
             ),
             new Starship(
                 2,
                 'USS Espresso (NCC-1234-C)',
                 'Latte',
                 'James T. Quick!',
-                'repaired',
+                StarshipStatusEnum::COMPLETED
             ),
             new Starship(
                 3,
                 'USS Wanderlust (NCC-2024-W)',
                 'Delta Tourist',
                 'Kathryn Journeyway',
-                'under construction',
+                StarshipStatusEnum::WAITING
             ),
         ];
     }
+
     public function find(int $id): ?Starship
     {
-        foreach($this->findAll() as $starship){
+        foreach ($this->findAll() as $starship) {
             if ($starship->getId() === $id) {
                 return $starship;
             }
         }
+
         return null;
     }
 }
